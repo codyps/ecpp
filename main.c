ba7af5fd6c7702ef5ea95a0f9e797d3f548e7fe5
Cody P Schafer <devel@codyps.com>
2014-10-10 21:56:40 -0700

    sitting in the dir

--- main.c
+++ main.c
@@ -1,3 +1,24 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* tokenizer gets arrays of bytes, and each time emit 0 or more tokens */
+struct token {
+	const char *token;
+	size_t token_len;
+	size_t alloc_len;
+};
+
+struct tokenizer {
+	struct list_head input_buffers;
+	struct list_head tokens;
+};
+
+struct tokentok_feed(struct tokenizer *tok, char *data, size_t data_len)
+{
+
+}
+
 int main(int argc, char **argv)
 {
 	return 0;
